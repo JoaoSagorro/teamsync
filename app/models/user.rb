106f48d1a73cc3ebd168c52c94912a3b0a9000eb
Joao Sagorro <joao.sagorro@gmail.com>
2023-11-28 14:12:41 +0000

    seeds all done, without images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :teams
+  has_many :teams, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
