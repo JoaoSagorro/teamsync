87e25b414c4c73626b56fd4db866ed448e20a176
Kirsty Mason <kirsty_mason9@hotmail.com>
2023-11-30 16:20:17 +0000

    rfer

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,6 @@ class Event < ApplicationRecord
   has_many :employees, through: :event_employees
   accepts_nested_attributes_for :event_players
 
-  PLAYERS = Player.all.map { |player| "#{player.first_name} #{player.last_name}" }
-
   validates :end_date, comparison: { greater_than: :start_date }, presence: true
   validates :location, :title, :description, :start_date, presence: true
-  # validates :player, inclusion: { in: PLAYERS }
 end
